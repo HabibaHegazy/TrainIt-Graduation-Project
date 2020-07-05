ecf3c10b229e1a54f55f80dc11ea656ba72f7a77
Habiba hegazy <habiba.hhegazy1998@gmail.com>
2020-07-05 19:28:52 +0200

    Kinect Skeleton Tracking Code
    This is mainly a ready made code for Kinect SDK for Skeleton Tracking for the main joints needed in the project.

---
+++ Kinect/SkeletalTracking/SkeletalTracking/Class1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace SocketKinect
+{
+    public partial class Class1
+    {
+
+        public Class1()
+        { }
+
+        public void ClassElUDP()
+        {
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[1];
+            //IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 12345);
+
+            UdpClient udpClient = new UdpClient(12345);
+            try
+            {
+                udpClient.Connect("", 12345);
+
+                // Sends a message to the host to which you have connected.
+                Byte[] sendBytes = Encoding.ASCII.GetBytes("Is anybody there?");
+
+                udpClient.Send(sendBytes, sendBytes.Length);
+
+                // Sends a message to a different host using optional hostname and port parameters.
+                //IPEndPoint object will allow us to read datagrams sent from any source.
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 12345);
+
+                // Blocks until a message returns on this socket from a remote host.
+                Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+                string returnData = Encoding.ASCII.GetString(receiveBytes);
+
+                // Uses the IPEndPoint object to determine which of these two hosts responded.
+                Console.WriteLine("This is the message you received " + returnData.ToString());
+                Console.WriteLine("This message was sent from " + RemoteIpEndPoint.Address.ToString() 
+                                                    + " on their port number " + RemoteIpEndPoint.Port.ToString());
+
+                udpClient.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+        }
+
+
+        public void ClassElFunction(ArrayList data)
+        {
+            try
+            {
+
+                // Establish the remote endpoint 
+                // for the socket. This example 
+                // uses port 11111 on the local 
+                // computer. 
+                IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddr = ipHost.AddressList[1];
+                IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 12345);
+
+                // Creation TCP/IP Socket using 
+                // Socket Class Costructor 
+                Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+
+                    // Connect Socket to the remote 
+                    // endpoint using method Connect() 
+                    sender.Connect(localEndPoint);
+
+                    // We print EndPoint information 
+                    // that we are connected 
+                    Console.WriteLine("Socket connected to -> {0} ", sender.RemoteEndPoint.ToString());
+
+                    // Creation of messagge that 
+                    // we will send to Server 
+                    int[] array = new int[2];
+
+                    foreach (int i in data)
+                    {
+                        byte[] messageSent = BitConverter.GetBytes(i);
+                        int byteSent = sender.Send(messageSent);
+                        messageSent = null;
+                    }
+
+                    // Data buffer 
+                    byte[] messageReceived = new byte[1024];
+
+                    // We receive the messagge using the method Receive(). 
+                    //This method returns number of bytes received, that we'll use to convert them to string 
+                    int byteRecv = sender.Receive(messageReceived);
+                    Console.WriteLine("Message from Server -> {0}",
+                        Encoding.ASCII.GetString(messageReceived,
+                                                    0, byteRecv));
+
+                    // Close Socket using the method Close() 
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+                }
+
+                // Manage of Socket's Exceptions 
+                catch (ArgumentNullException ane)
+                {
+
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+
+                catch (SocketException se)
+                {
+
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+            }
+
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine(data[0]);
+        }
+
+
+    }
+
+
+
+
+}
---
+++ Kinect/SkeletalTracking/SkeletalTracking/MainWindow.xaml.cs
@@ -0,0 +1,243 @@
+﻿// (c) Copyright Microsoft Corporation.
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
+// All other rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+using Coding4Fun.Kinect.Wpf;
+using SocketKinect;
+using System.Collections;
+
+namespace SkeletalTracking
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static ArrayList al = new ArrayList();
+        public static Class1 n = new Class1();
+        public MainWindow()
+        {
+            InitializeComponent();
+            n.ClassElUDP();
+
+            //Thread thr = new Thread(new ThreadStart(x.ClassElFunction(data)));
+            // thr.Start();
+        }
+
+        bool closing = false;
+        const int skeletonCount = 6;
+        Skeleton[] allSkeletons = new Skeleton[skeletonCount];
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) // kinect is opened
+        {
+            kinectSensorChooser1.KinectSensorChanged += new DependencyPropertyChangedEventHandler(kinectSensorChooser1_KinectSensorChanged);
+        }
+
+        void kinectSensorChooser1_KinectSensorChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            KinectSensor old = (KinectSensor)e.OldValue;
+
+            StopKinect(old);
+
+            KinectSensor sensor = (KinectSensor)e.NewValue;
+
+            if (sensor == null)
+            {
+                return;
+            }
+
+            var parameters = new TransformSmoothParameters {
+                Smoothing = 0.3f,
+                Correction = 0.0f,
+                Prediction = 0.0f,
+                JitterRadius = 1.0f,
+                MaxDeviationRadius = 0.5f
+            };
+            sensor.SkeletonStream.Enable(parameters);
+
+            sensor.SkeletonStream.Enable();
+
+            sensor.AllFramesReady += new EventHandler<AllFramesReadyEventArgs>(sensor_AllFramesReady);
+            sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+            sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+
+            try
+            {
+                sensor.Start();
+            }
+            catch (System.IO.IOException)
+            {
+                kinectSensorChooser1.AppConflictOccurred();
+            }
+        }
+
+        void sensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            if (closing)
+            {
+                return;
+            }
+
+            //Get a skeleton
+            Skeleton first = GetFirstSkeleton(e);
+
+            if (first == null)
+            {
+                return;
+            }
+
+
+
+            //set scaled position
+            //ScalePosition(headImage, first.Joints[JointType.Head]);
+            ScalePosition(leftEllipse, first.Joints[JointType.HandLeft]);
+            ScalePosition(rightEllipse, first.Joints[JointType.ShoulderRight]);
+            ScalePosition(Brown, first.Joints[JointType.ElbowRight]);
+
+            GetCameraPoint(first, e);
+
+        }
+
+        void GetCameraPoint(Skeleton first, AllFramesReadyEventArgs e)
+        {
+
+            using (DepthImageFrame depth = e.OpenDepthImageFrame())
+            {
+                if (depth == null ||
+                    kinectSensorChooser1.Kinect == null)
+                {
+                    return;
+                }
+
+                //Map a joint location to a point on the depth map
+                //head
+                DepthImagePoint headDepthPoint =
+                    depth.MapFromSkeletonPoint(first.Joints[JointType.Head].Position);
+
+                //left hand
+                DepthImagePoint leftDepthPoint =
+                    depth.MapFromSkeletonPoint(first.Joints[JointType.HandLeft].Position);
+
+                Console.WriteLine(first.Joints[JointType.HandLeft].Position.X);
+                //Console.WriteLine(leftDepthPoint.X);
+
+                //right hand
+                DepthImagePoint rightDepthPoint =
+                    depth.MapFromSkeletonPoint(first.Joints[JointType.ShoulderRight].Position);
+
+                DepthImagePoint ElbowDepthPoint =
+                    depth.MapFromSkeletonPoint(first.Joints[JointType.ElbowRight].Position);
+
+                //Map a depth point to a point on the color image
+                //head
+                ColorImagePoint headColorPoint =
+                    depth.MapToColorImagePoint(headDepthPoint.X, headDepthPoint.Y,
+                    ColorImageFormat.RgbResolution640x480Fps30);
+                //left hand
+                ColorImagePoint leftColorPoint =
+                    depth.MapToColorImagePoint(leftDepthPoint.X, leftDepthPoint.Y,
+                    ColorImageFormat.RgbResolution640x480Fps30);
+                //right hand
+                ColorImagePoint rightColorPoint =
+                    depth.MapToColorImagePoint(rightDepthPoint.X, rightDepthPoint.Y,
+                    ColorImageFormat.RgbResolution640x480Fps30);
+
+                ColorImagePoint ElbowColorPoint =
+                    depth.MapToColorImagePoint(ElbowDepthPoint.X, ElbowDepthPoint.Y,
+                    ColorImageFormat.RgbResolution640x480Fps30);
+
+                //Set location
+                CameraPosition(headImage, headColorPoint);
+                CameraPosition(leftEllipse, leftColorPoint);
+                CameraPosition(rightEllipse, rightColorPoint);
+                CameraPosition(Brown, ElbowColorPoint);
+            }
+        }
+
+        Skeleton GetFirstSkeleton(AllFramesReadyEventArgs e) // 2wel skelaton 2tla2t
+        {
+            using (SkeletonFrame skeletonFrameData = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrameData == null)
+                {
+                    return null;
+                }
+
+                skeletonFrameData.CopySkeletonDataTo(allSkeletons);
+
+                //get the first tracked skeleton
+                Skeleton first = (from s in allSkeletons
+                                  where s.TrackingState == SkeletonTrackingState.Tracked
+                                  select s).FirstOrDefault();
+
+                return first;
+
+            }
+        }
+
+        private void StopKinect(KinectSensor sensor)
+        {
+            if (sensor != null)
+            {
+                if (sensor.IsRunning)
+                {
+                    //stop sensor 
+                    sensor.Stop();
+
+                    //stop audio if not null
+                    if (sensor.AudioSource != null)
+                    {
+                        sensor.AudioSource.Stop();
+                    }
+                }
+            }
+        }
+
+        private void CameraPosition(FrameworkElement element, ColorImagePoint point)
+        {
+            // Divide by 2 for width and height so point is right in the middle instead of in top/left corner
+            Canvas.SetLeft(element, point.X - element.Width / 2);
+            Canvas.SetTop(element, point.Y - element.Height / 2);
+        }
+
+        private void ScalePosition(FrameworkElement element, Joint joint)
+        {
+            //convert the value to X/Y
+            //Joint scaledJoint = joint.ScaleTo(1280, 720); 
+
+            //convert & scale (.3 = means 1/3 of joint distance)
+            Joint scaledJoint = joint.ScaleTo(1280, 720, .3f, .3f);
+
+            Canvas.SetLeft(element, scaledJoint.Position.X);
+            Canvas.SetTop(element, scaledJoint.Position.Y);
+
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            closing = true;
+            StopKinect(kinectSensorChooser1.Kinect);
+        }
+
+        private void KinectSkeletonViewer_Loaded(object sender, RoutedEventArgs e)
+        {}
+    }
+}
