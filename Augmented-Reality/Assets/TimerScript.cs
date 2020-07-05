6c807684fc475dc5d9406fc26f40e4f274882ed9
Habiba hegazy <habiba.hhegazy1998@gmail.com>
2020-07-05 19:16:01 +0200

    augmented reality
    The AR code is made using Unity and was used on AR glasses connected to the android application

---
+++ Augmented-Reality/Assets/TimerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TimerScript : MonoBehaviour
+{
+    public static float timer;
+    public TextMeshPro txt;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txt = GetComponent<TextMeshPro>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        string minutes = Mathf.Floor(timer / 60).ToString("00");
+        string seconds = (timer % 60).ToString("00");
+
+        txt.SetText(minutes + " : " + seconds);
+
+
+    }
+}
